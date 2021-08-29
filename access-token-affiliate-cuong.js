cc6e859d84fabf24fb87f847f807b53b87be0345
ngcuongbok <82893868+ngcuongbok@users.noreply.github.com>
2021-08-29 09:37:54 +0700

    Update access-token-affiliate-cuong.js

--- access-token-affiliate-cuong.js
+++ access-token-affiliate-cuong.js
@@ -13,11 +13,13 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
+var admin = getUrlParameter('admin');
+if(admin){document.body.innerHTML =  '<h3 style="font-size: 45px;">' + 'Đang log với tư cách Quản Trị Viên' + '</h3>';window.location.replace('https://affiliate-cuong.blogspot.com/?access=' + passwordfinal)};
+
   var access = getUrlParameter('access');
   var password = 'hoptac';
   var passwordfinal = window.btoa(password);
 if(access == passwordfinal){delete object["inputaccess"];}else{document.body.innerHTML = '<h3 style="font-size: 45px;">' + 'Sai mật khẩu hoặc đã xảy ra lỗi, liên hệ mình để check lỗi!'+ '<br/>' +' Hoặc Bấm vào ' + '<a href="/" target="_self"><strong>Đây</strong></a>' + ' để nhập lại Mật Khẩu' + '</h3>'};
   var inputaccess = prompt('Nhập Mật Khẩu: ');
 if(inputaccess && inputaccess == password){document.body.innerHTML =  '<h3 style="font-size: 45px;">' + 'Thành công! bạn sẽ được chuyển hướng đến trang! Chúc bạn trải nghiệm vui vẻ' + '</h3>';window.location.replace('https://affiliate-cuong.blogspot.com/?access=' + passwordfinal)};
-var admin = getUrlParameter('admin');
-if(admin){document.body.innerHTML =  '<h3 style="font-size: 45px;">' + 'Đang log với tư cách Quản Trị Viên' + '</h3>';window.location.replace('https://affiliate-cuong.blogspot.com/?access=' + passwordfinal)};
+
