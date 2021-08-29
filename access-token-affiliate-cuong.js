ac835beea72e22591797da661b36416f582ee056
ngcuongbok <82893868+ngcuongbok@users.noreply.github.com>
2021-08-29 09:16:18 +0700

    Update access-token-affiliate-cuong.js

--- access-token-affiliate-cuong.js
+++ access-token-affiliate-cuong.js
@@ -15,5 +15,8 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
   var access = getUrlParameter('access');
   var password = 'hoptacphattrien';
-  var passwordfinal = window.btoa('hoptacphattrien');
-  alert(passwordfinal)
+  var passwordfinal = window.btoa(password);
+if(access == passwordfinal){delete object["inputaccess"];}else{document.body.innerHTML = '<h3 style="font-size: 45px;">' + 'Sai mật khẩu hoặc đã xảy ra lỗi, liên hệ mình để check lỗi!'+ '<br/>' +' Hoặc Bấm vào ' + '<a href="/" target="_self"><strong>Đây</strong></a>' + ' để nhập lại Mật Khẩu' + '</h3>'}
+  var inputaccess = prompt('Nhập Mật Khẩu: ');
+if(inputaccess && inputaccess == password){window.location.replace('https://affiliate-cuong.blogspot.com/?access=' + passwordfinal)}
+
